ee9e033119bfbf042d44b03ad99fdac79eae3cf6
lengGod <galangsamudro264@gmail.com>
2025-11-04 00:53:21 +0700

    error page 403 fix bug

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -2,5 +2,5 @@
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Akses Dilarang'))
+@section('message', __(isset($exception) ? $exception->getMessage() : 'Akses Dilarang'))
 @section('description', 'Anda tidak memiliki izin untuk mengakses sumber daya ini.')
