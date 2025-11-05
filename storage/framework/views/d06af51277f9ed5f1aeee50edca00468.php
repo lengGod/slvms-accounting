48e596d420d236f71a1b1aaedc98c2d4b5aa3dd0
lengGod <galangsamudro264@gmail.com>
2025-11-05 13:07:09 +0700

    delete view

--- storage/framework/views/d06af51277f9ed5f1aeee50edca00468.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('title', __('Halaman Tidak Ditemukan')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Halaman Tidak Ditemukan')); ?>
-<?php $__env->startSection('description', 'Maaf, kami tidak dapat menemukan halaman yang Anda cari. Mungkin tautan tersebut rusak atau halaman telah dihapus.'); ?>
-
-<?php echo $__env->make('errors::layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\laragon\www\slv-acounting\resources\views/errors/404.blade.php ENDPATH**/ ?>
